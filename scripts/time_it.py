cfa573e5eb18e1c4bb2a343cd75adf9b19780656
ZQRL (Søren Lund) <zqrl@novonordisk.com>
2021-01-04 14:32:33 +0100

    Refactored

--- scripts/time_it.py
+++ scripts/time_it.py
@@ -1,29 +1,38 @@
 #!/usr/bin/env python3
 """
-Measure execution time of program
+Run a program and then print the number seconds the program ran.
 """
-from os.path import basename
+
 import subprocess
-from timeit import default_timer as now
+
 from datetime import timedelta
+from timeit import default_timer as now
 
-def main(args):
-    """
-    Parse arguments and execute script
-    """
-    script_name = basename(args.pop(0))
 
-    if len(args) > 0:
-        command = str.join(' ', args)
-        print('Running: ', command)
-    else:
-        print('USAGE: ', script_name, ' command')
-        sys.exit(1)
+def time_it(program_and_args, verbose=False):
+    """Returns the time (in seconds) it takes to run a program."""
 
-    start = now()
-    subprocess.call(command, shell=True)
-    print(timedelta(seconds=now()-start))
+    if len(program_and_args) == 0:
+        raise ValueError('program_and_args must contain at least one element')
+
+    program = str.join(' ', program_and_args)
+
+    if verbose:
+        print(f"Running: {program}")
+
+    start_time = now()
+
+    subprocess.call(program, shell=True)
+
+    return timedelta(seconds=now()-start_time).total_seconds()
+
 
 if __name__ == '__main__':
     import sys
-    main(sys.argv)
+    from os.path import basename
+
+    if len(sys.argv) == 1:
+        print(f"USAGE: {basename(sys.argv[0])} program [program args...]")
+        sys.exit(1)
+
+    print(f'Elapsed: {time_it(sys.argv[1:], verbose=True):.2f} s')
